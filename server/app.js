9f890f0c7535b618963030a9110ffa6f7a51bddc
Ramandeep kaur <157648072+Ramandeepbirdi@users.noreply.github.com>
2024-10-25 13:25:59 -0400

    Add files via upload

--- server/app.js
+++ server/app.js
@@ -6,24 +6,28 @@ app.use(cors());
 
 const PORT = 5000;
 
-// Sample resume data
+
 const resumeData = {
   education: [
     { school: "Humber College", degree: "Diploma Computer Programming", year: "2024-2025" },
-    { school: "Guru Nanak Dev polytechnic College", degree: "Diploma in Computer Science", year: "2020-2024" }
+    { school: "Guru Nanak Dev Polytechnic College", degree: "Diploma in Computer Science", year: "2020-2024" }
   ],
   projects: [
-    { title: "Botanix Brain", description: "A project is based on Python programming language and it is made for the contribution of Green revolution" },
+    { title: "Botanix Brain", description: "A project based on Python programming language made for the contribution to the Green Revolution." },
     { title: "React-Express Portfolio", description: "A portfolio website built with React and Express." }
+  ],
+  services: [
+    { title: "WP Developing", description: "Creating WordPress websites with a focus on usability and aesthetics." },
+    { title: "UI/UX Design", description: "Designing user interfaces and experiences that are engaging and user-friendly." },
+    { title: "Web Design", description: "Developing responsive and modern websites." },
+    { title: "SEO Optimization", description: "Enhancing website visibility on search engines." },
   ]
 };
 
-// Routes
 
 app.get('/', (req, res) => {
   res.send('Welcome to the API');
 });
-
 
 app.get('/getEdu', (req, res) => {
   res.json(resumeData.education);
@@ -31,6 +35,11 @@ app.get('/getEdu', (req, res) => {
 
 app.get('/getProjects', (req, res) => {
   res.json(resumeData.projects);
+});
+
+
+app.get('/getServices', (req, res) => {
+  res.json(resumeData.services);
 });
 
 app.listen(PORT, () => {
